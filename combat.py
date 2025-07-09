c45fc6bd1f829d5694f88b30bbbdfbbb3a7d36a7
PythonTheMage <104693447+PythonTheMage@users.noreply.github.com>
2025-07-09 02:05:24 -0700

    1.0
    This is the first functional version of the script.

---
+++ combat.py
@@ -0,0 +1,60 @@
+import time
+import random
+import os
+
+messageDelay = 0.8
+playerHealth = 20
+enemyHealth = 10
+
+def playerTurn():
+    global enemyHealth
+    global playerHealth
+    playerInput = input("What do you do? ")
+    print(f"\nYou {playerInput} the Goblin.")
+    if playerInput.lower() == "attack":
+        time.sleep(messageDelay)
+        playerAttack = random.randint(0,4)
+        enemyHealth -= playerAttack
+        print(f"Goblin takes {playerAttack} damage!")
+        if enemyHealth <= 0:
+            time.sleep(messageDelay)
+            print("You killed the Goblin!")
+            time.sleep(messageDelay)
+            exit()
+    elif playerInput.lower() == "run":
+        print("You escaped!")
+        exit()
+    else:
+        time.sleep(messageDelay)
+        print("The Goblin just stares at you...")
+        time.sleep(2)
+        playerTurn()
+        
+def enemyTurn():
+    global enemyHealth
+    global playerHealth
+    enemyAttack = random.randint(0,5)
+    print(f"The goblin attacks you.")
+    time.sleep(messageDelay)
+    print(f"You took {enemyAttack} damage!")
+    playerHealth -= enemyAttack
+    
+def newTurn():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+    
+def main():
+    input("Press ENTER to Start Combat Simulation...")
+    newTurn()
+    print("A Goblin blocks your path!")
+    while playerHealth > 0 and enemyHealth > 0:
+        print(f"\nYOU: {playerHealth} | ENEMY: {enemyHealth}")
+        playerTurn()
+        time.sleep(messageDelay)
+        enemyTurn()
+        time.sleep(messageDelay)
+        newTurn()
+
+main()
